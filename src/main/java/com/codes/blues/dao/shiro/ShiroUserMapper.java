b18e4b18b79b99988ba4ab1c5c72991e0f0ffdd9
blueslin0711 <649214911@qq.com>
2020-04-14 10:43:27 +0800

    shiro 权限管理集成 优化

--- src/main/java/com/codes/blues/dao/shiro/ShiroUserMapper.java
+++ src/main/java/com/codes/blues/dao/shiro/ShiroUserMapper.java
@@ -1,9 +1,10 @@
 package com.codes.blues.dao.shiro;
 
 import com.codes.blues.dao.shiro.model.ShiroUser;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface ShiroUserMapper {
-    ShiroUser getShiroUserByUserId(String id);
+    ShiroUser getShiroUserByUserId(@Param("id") String id);
 }
